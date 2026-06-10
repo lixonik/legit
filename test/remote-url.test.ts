b0499b6b492fc0f6454fe22485b3fff38287f1c5
lixonik <deimos.75@mail.ru>
2026-06-10 08:43:00 +0300

    Test remote-url edge cases (subgroups, http upgrade, branch encoding)

--- test/remote-url.test.ts
+++ test/remote-url.test.ts
@@ -11,6 +11,15 @@ describe('toWebUrl', () => {
   it('ssh://', () => {
     expect(toWebUrl('ssh://git@gitlab.com/u/r.git')).toBe('https://gitlab.com/u/r');
   });
+  it('scp shorthand with nested subgroups', () => {
+    expect(toWebUrl('git@gitlab.com:group/sub/repo.git')).toBe('https://gitlab.com/group/sub/repo');
+  });
+  it('https without a .git suffix', () => {
+    expect(toWebUrl('https://github.com/u/r')).toBe('https://github.com/u/r');
+  });
+  it('upgrades http to https', () => {
+    expect(toWebUrl('http://example.com/u/r.git')).toBe('https://example.com/u/r');
+  });
   it('empty or invalid', () => {
     expect(toWebUrl('')).toBe('');
     expect(toWebUrl('not-a-url')).toBe('');
@@ -23,5 +32,14 @@ describe('commit and file urls', () => {
   });
   it('file url', () => {
     expect(fileWebUrl('https://github.com/u/r', 'main', 'src/a.ts')).toBe('https://github.com/u/r/blob/main/src/a.ts');
+  });
+  it('encodes a branch name with a slash in the file url', () => {
+    expect(fileWebUrl('https://github.com/u/r', 'feature/x', 'a.ts')).toBe(
+      'https://github.com/u/r/blob/feature%2Fx/a.ts',
+    );
+  });
+  it('returns empty when there is no web base', () => {
+    expect(commitWebUrl('', 'abc123')).toBe('');
+    expect(fileWebUrl('', 'main', 'a.ts')).toBe('');
   });
 });
